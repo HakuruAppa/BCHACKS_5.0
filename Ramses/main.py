5f9c0165d9bec873624a5923531ca543870ca808
nurture <132051798+esper777@users.noreply.github.com>
2024-01-27 19:34:13 -0800

    telebot's cœur
    added /help command 
    added /status command, not fully done
    added /register command, not working fully!
    user-friendly fixes and implementations

--- Ramses/main.py
+++ Ramses/main.py
@@ -1,29 +1,68 @@
 from typing import Final
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler
 
 TOKEN: Final = '6818698464:AAExduA8qUwEWa3wl1i_M2gK3OVF2Uv0-Fk'
 BOT_USERNAME: Final = '@EunaiBot'
+NAME, ID_NUMBER = range(2)
+#Arrays
+commands = [
+    ('/start', 'Intializes the bot'), ('/help', 'Lists all known commands'), ('/status', 'Provides your current health status'), 
+    ('/register', 'Intiates registration process'), ('regCancel', 'Cancels the registration process'),
+
+]
+
+#Misc functions, formatting etc
+def format_help_command(commands):
+    return '\n'.join([f'{command}: {description}' for command, description in commands])
 
 #Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello!, I am your healthbot.')
+    await update.message.reply_text('Hello!, I am Eunai. How can I help you today?\n')
+    await update.message.reply_text('Please use the /register command to link Eunai with your data')
+    await update.message.reply_text('Type /help for the list of known commands.')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Here are the list of commands I know')
-
+    commands_list = format_help_command(commands)
+    help_message = ('Here are the list of commands I know')
+    await update.message.reply_text(help_message)
+    await update.message.reply_text(commands_list)
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Custom Command')
 
 async def status_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('---STATUS---')
+    intro = ('Please wait while I pull your data')
+    await update.message.reply_text(intro)
+    
+#Registration command, and linking with convo handler
+async def register_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Beginning registration process')
+    return NAME
 
+async def get_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_name = update.message.text
+    context.user_data['name'] = user_name
+    await update.message.reply_text(f'Thank you {user_name}. Please enter your ID NUMBER assigned by the Space Health Ministry:')
+    return ID_NUMBER
+
+async def get_id_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.text
+    context.user_data['id_number'] = user_id
+    completion_msg = f'Registration is now complete.\nVerify your information. If you need to make changes, re-initiate the registration process' 
+    userId_msg = f'\n Username: {context.user_data["name"]}\nID_Number: {user_id}'
+    await update.message.reply_text(completion_msg)
+    await update.message.reply_text(userId_msg)
+    return ConversationHandler.END
+
+async def regCancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Registration has been cancelled...\nComplete registration to continue using Eunai')
+    return ConversationHandler.END #ends convo
 #Responses 
 def handle_response(text: str) -> str:
     processed: str = text.lower()
-    if 'hello'.toLowerCase() in text:
+    if 'hello' in processed:
         return 'hello there!'
-    if 'help' in text:
+    if 'help' in processed:
         return 'use the command for help'
     
     return 'I dont understand... please use the help command'
@@ -58,6 +97,17 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('custom', custom_command))
     app.add_handler(CommandHandler('status', status_command))
+    
+    #Conversation Handler
+    convo_handler = ConversationHandler(
+        entry_points = [CommandHandler('register', register_command)],
+        states ={
+            NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name)],
+            ID_NUMBER: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_id_number)]
+        },
+        fallbacks = [CommandHandler('regCancel', regCancel)]
+    )
+    app.add_handler(convo_handler)
 
     #Messages
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
